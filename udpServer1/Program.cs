a002bb506c809a341e1995e357e8e7ec3c008b3c
SamVeez <49760035+SamVeez@users.noreply.github.com>
2020-05-11 12:36:13 +0300

    some code

---
+++ udpServer1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Practical1Server
+{
+    public class UdpFileServer
+    {
+        private static IPAddress ipAddress;
+        private const int port = 777;
+        private static UdpClient sender = new UdpClient();
+        private static IPEndPoint ipEndPoint;
+
+        private static FileStream fileStream;
+
+        public static void Main()
+        {
+            try
+            {
+                Console.Write("IP ");
+                ipAddress = IPAddress.Parse(Console.ReadLine());
+                ipEndPoint = new IPEndPoint(ipAddress, port);
+                Console.Write("путь ");
+                fileStream = new FileStream(Console.ReadLine(), FileMode.Open, FileAccess.Read);
+                SendFile();
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private static void SendFile()
+        {
+            var bytes = new byte[fileStream.Length];
+            fileStream.Read(bytes, 0, bytes.Length);
+
+            Console.WriteLine("размер " + fileStream.Length + " байт, отправляем");
+            try
+            {
+                sender.Send(bytes, bytes.Length, ipEndPoint);
+                fileStream.Close();
+                sender.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                fileStream.Close();
+                sender.Close();
+            }
+            Console.WriteLine("отправлено");
+            Console.Read();
+        }
+    }
+}
