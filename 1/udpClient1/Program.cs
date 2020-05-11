0f056eda209bed0627b61becd7a42cb5fec85562
SamVeez <49760035+SamVeez@users.noreply.github.com>
2020-05-11 12:36:54 +0300

    some code

---
+++ 1/udpClient1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace udpClient1
+{
+    public class udpClient1
+    {
+        private const int port = 777;
+        private static readonly UdpClient recUdpClient = new UdpClient(port);
+        private static IPEndPoint ipEndPoint;
+        private static byte[] receiveBytes = new byte[0];
+
+        public static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Ждем файл");
+                receiveBytes = recUdpClient.Receive(ref ipEndPoint);
+                using (var fileStream = new FileStream("temp.txt", FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite))
+                {
+                    fileStream.Write(receiveBytes, 0, receiveBytes.Length);
+                    Process.Start(fileStream.Name);
+                }
+                recUdpClient.Close();
+                Console.Read();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                recUdpClient.Close();
+                Console.Read();
+            }
+        }
+    }
+}
